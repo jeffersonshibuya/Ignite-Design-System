755f1e195355025877ccae29378dc352eaa3537c
jefferson shibuya <Jefferson Shibuya>
2022-10-13 11:00:21 -0300

    Initial

---
+++ packages/docs/src/stories/TextArea.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Box, Text, TextArea, TextAreaProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/TextArea Area',
+  component: TextArea,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: 2 }}
+        >
+          <Text size="sm">Description</Text>
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+} as Meta<TextAreaProps>
+
+export const Primary: StoryObj<TextAreaProps> = {
+  args: {
+    placeholder: 'Add a description',
+  },
+}
+
+export const disabled: StoryObj<TextAreaProps> = {
+  args: {
+    disabled: true,
+  },
+}
